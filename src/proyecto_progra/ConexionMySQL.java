3e9b49308dfb06ac8aacda8eed55fa4554ebe4e8
Maxt1165 <maoa.olayanastacio@gmail.com>
2025-07-15 20:04:48 -0500

    a

---
+++ src/proyecto_progra/ConexionMySQL.java
@@ -0,0 +1,16 @@
+package proyecto_progra;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionMySQL {
+    private static final String URL = "jdbc:mysql://localhost:3306/ConsumoSupermercado";
+    private static final String USER = "root";
+    private static final String PASS = "MaxAl1165";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
+
