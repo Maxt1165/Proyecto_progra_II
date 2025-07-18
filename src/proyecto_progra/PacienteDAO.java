d807348319c20c4a4c69d22a8b0a6dbf0d6fec6f
LeslyLotusPhoneix <leslymunozalberca@gmail.com>
2025-07-17 21:03:49 -0500

    Update PacienteDAO.java

--- src/proyecto_progra/PacienteDAO.java
+++ src/proyecto_progra/PacienteDAO.java
@@ -7,18 +7,9 @@ public class PacienteDAO {
 
     public static boolean insertarPaciente(Paciente paciente) {
         String sql = "INSERT INTO Pacientes (DNI, Nombre, Apellido, Sexo, FechaNac, FechaReg, Domicilio) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        try (Connection conn = ConexionMySQL.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql)) {
-
-            ps.setString(1, paciente.getDni());
-            ps.setString(2, paciente.getNombre());
-            ps.setString(3, paciente.getApellidos());
-            ps.setString(4, paciente.getSexo());
-            ps.setDate(5, paciente.getFechaNacimiento());
-            ps.setDate(6, paciente.getFechaRegistro());
-            ps.setString(7, paciente.getDomicilio());
-           
-                    int respuesta = JOptionPane.showConfirmDialog(
+        
+        //Validación con el usuario
+           int respuesta = JOptionPane.showConfirmDialog(
                         null,
                         "¿Está seguro que desea ingresar al paciente: " + paciente.toString(),
                         "Confirmar cambio",
@@ -27,15 +18,26 @@ public class PacienteDAO {
                     );
                     
                     if (respuesta != JOptionPane.YES_OPTION) {
-                        JOptionPane.showMessageDialog(null, "Revise sus datos");
-                    }else{
-                        int filasInsertadas = ps.executeUpdate();
-                            return filasInsertadas > 0;                 
-                   }
-        return false;
-
+                        JOptionPane.showMessageDialog(null, "Operaci+on cancelada");
+                        return false;
+                    }
+                
+        try (Connection conn = ConexionMySQL.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)) {
+                
+            ps.setString(1, paciente.getDni());
+            ps.setString(2, paciente.getNombre());
+            ps.setString(3, paciente.getApellidos());
+            ps.setString(4, paciente.getSexo());
+            ps.setDate(5, paciente.getFechaNacimiento());
+            ps.setDate(6, paciente.getFechaRegistro());
+            ps.setString(7, paciente.getDomicilio());
+           
+            int filasInsertadas = ps.executeUpdate();
+            return filasInsertadas > 0;                 
+        
         } catch (SQLException e) {
-            System.err.println("Error al insertar paciente: " + e.getMessage());
+            System.err.println("Error al insertar paciente: " + e.getMessage()); 
             return false;
         }
     }
